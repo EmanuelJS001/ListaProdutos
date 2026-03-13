db8bfff59c6e5acd6bbcc66ea11782045d265348
EmanuelJS001 <emanuelJS.001@gmail.com>
2026-03-12 21:13:09 -0300

    adicionando imagens

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {View, FlatList, StyleSheet, Text, StatusBar} from 'react-native';
+import {View, FlatList, StyleSheet, Text, StatusBar, Image, ImageComponent} from 'react-native';
 
 const DATA = [
   {
@@ -6,44 +6,55 @@ const DATA = [
     nome: 'Colisor de Hádrons',
     valor: '4.750.000.000',
     dtCadastro: '15/03/2025',
-    qntd: '2'
+    qntd: '2',
+    img: require("./imgs/colisor.jpg")
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
     nome: 'Detector de partículas',
     valor: '500.000.000',
     dtCadastro: '19/04/2025',
-    qntd: '10'
+    qntd: '10',
+    img: require("./imgs/detector.jpeg")
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
     nome: 'Ímã supercondutor',
     valor: '150.000',
     dtCadastro: '19/04/2025',
-    qntd: '800'
+    qntd: '800',
+    img: require("./imgs/ima.jpg")
   },
     {
     id: '67aebcbd-94b2-441f-92f1-f65257fd79d6',
     nome: 'Sistema de criogenia',
     valor: '50.000.000',
     dtCadastro: '02/05/2025',
-    qntd: '20'
+    qntd: '20',
+    img: require("./imgs/criogenia.jpg")
   },  
     {
     id: '233c8bcd-a653-45e8-ba2e-03be895ea563',
     nome: 'Sistema de computação para análise de dados',
     valor: '10.000.000',
     dtCadastro: '25/05/2025',
-    qntd: '50'
+    qntd: '50',
+    img: require("./imgs/computacao.jpg")
   },
 ];
 
-const Item = ({nome, valor, dtCadastro, qntd}) => (
+const Item = ({nome, valor, dtCadastro, qntd, img}) => (
   <View style={styles.item}>
-    <Text style={styles.nome}>{nome}</Text>
-    <Text style={styles.nome }>{valor}</Text>
-    <Text style={styles.nome}>{dtCadastro}</Text>
-    <Text style={styles.nome}>{qntd}</Text>
+
+    <Image source={img} style={styles.imagem} />
+
+    <View style={styles.texto}>
+      <Text style={styles.nome}>Nome: {nome}</Text>
+      <Text style={styles.nome }>Valor: ${valor}</Text>
+      <Text style={styles.nome}>Data de Cadastro: {dtCadastro}</Text>
+      <Text style={styles.nome}>Quantidade: {qntd}</Text>
+    </View>
+
   </View>
 );
 
@@ -52,7 +63,7 @@ return(
   <View style={styles.container}>
     <FlatList
       data={DATA}
-      renderItem={({item}) => <Item nome={item.nome} valor={item.valor} dtCadastro={item.dtCadastro} qntd={item.qntd}/>}
+      renderItem={({item}) => <Item nome={item.nome} valor={item.valor} dtCadastro={item.dtCadastro} qntd={item.qntd} img={item.img}/>}
       keyExtractor={item => item.id}
     />
   </View>
@@ -69,8 +80,22 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-  },
+    flexDirection:"row",
+    borderRadius: 30,
+  },  
   nome: {
-    fontSize: 32,
+    fontSize: 18,
+    marginTop: 10,
   },
+  texto:{
+    flexDirection: 'column',
+    justifyContent: "center",
+  },
+  imagem: {
+  width: 150,
+  height: 150,
+  marginRight: 20,
+  borderRadius: 80,
+},
+
 });
